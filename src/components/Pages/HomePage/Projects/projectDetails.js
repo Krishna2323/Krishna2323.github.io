4ab43b6129f7ea64a8ff419921cb66fddd429470
Krishna2323 <belivethatkg@gmail.com>
2022-12-10 02:48:20 +0530

    Projects Added.
    Signed-off-by: Krishna2323 <belivethatkg@gmail.com>

--- src/components/Pages/HomePage/Projects/projectDetails.js
+++ src/components/Pages/HomePage/Projects/projectDetails.js
@@ -1,5 +1,10 @@
 import Omnifood from "../../../assets/Images/Hero-Sections/Omnifood.png"
 import Forkify from "../../../assets/Images/Hero-Sections/Forkify.png"
+import Bankist from "../../../assets/Images/Hero-Sections/Bankist.png"
+import Trillo from "../../../assets/Images/Hero-Sections/Trillo.png"
+import Natours from "../../../assets/Images/Hero-Sections/Natours-CSS.png"
+import Nexter from "../../../assets/Images/Hero-Sections/Nexter.png"
+
 
 export const projects = [
   {
@@ -31,6 +36,49 @@ export const projects = [
     responsive: false,
     link:"https://quiet-travesseiro-698e66.netlify.app/",
     image:Forkify,
+    projectDescription:"This project was focused on learning JavaScript in-depth. It covered almost every concept of JavaScript. The most important concept for me was the OOP, MVC architecture, Advanced DOM, DOM updating algorithm, and asynchronous JavaScript.",
+    conceptsCovered: [
+      "JS Engine and Runtime",
+      "Execution Contexts",
+      "The Call Stack",
+      "The this Keyword",
+      "Primitives vs. Objects",
+      "Data Structures",
+      "Modern Operators",
+      "call, apply and bind",
+      "Numbers & Dates",
+      "Array Methods"
+    ],
+  },
+  {
+    title: "Bankist",
+    techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
+    timeSpent: "1 Week",
+    responsive: true,
+    link:"https://magical-paprenjak-e2a068.netlify.app/",
+    image:Bankist,
+    projectDescription:
+      "This project was focused on learning the DOM and DOM events. This project taught me how the DOM works behind the scenes. It covered important concepts like Event Propagation, Event Delegation, DOM Traversing, and The Intersection Observer API",
+    conceptsCovered: [
+      "Smooth Scrolling",
+      "Types of Events",
+      "Event Handlers",
+      "Bubbling and Capturing",
+      "Event Delegation",
+      "Passing Arguments to Handlers",
+      "The Scroll Event",
+      "Lazy Loading Images",
+      "Lifecycle DOM Events",
+      "Efficient Script Loading"
+    ],
+  },
+  {
+    title: "Trillo",
+    techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
+    timeSpent: "1 Week",
+    responsive: true,
+    link:"https://lively-kleicha-55527e.netlify.app/",
+    image:Trillo,
     projectDescription:
       "This project was focused on learning best practices with HTML, learning CSS from basics to some advanced technique and after that basics of SASS/SCSS. This project also tought me the techniques to build responsive websites using Grid/Flexbox.",
     conceptsCovered: [
@@ -47,12 +95,12 @@ export const projects = [
     ],
   },
   {
-    title: "Omnifood",
+    title: "Natours",
     techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
     timeSpent: "1 Week",
     responsive: true,
     link:"https://lively-kleicha-55527e.netlify.app/",
-    image:Omnifood,
+    image:Natours,
     projectDescription:
       "This project was focused on learning best practices with HTML, learning CSS from basics to some advanced technique and after that basics of SASS/SCSS. This project also tought me the techniques to build responsive websites using Grid/Flexbox.",
     conceptsCovered: [
@@ -69,56 +117,12 @@ export const projects = [
     ],
   },
   {
-    title: "Omnifood",
+    title: "Nexter",
     techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
     timeSpent: "1 Week",
     responsive: true,
     link:"https://lively-kleicha-55527e.netlify.app/",
-    image:Omnifood,
-    projectDescription:
-      "This project was focused on learning best practices with HTML, learning CSS from basics to some advanced technique and after that basics of SASS/SCSS. This project also tought me the techniques to build responsive websites using Grid/Flexbox.",
-    conceptsCovered: [
-      "HTML fundamentals",
-      "CSS fundamentals",
-      "Floats",
-      "Flexbox",
-      "Grids",
-      "Web Design Rules",
-      "Components Pattern",
-      "Layout Patern",
-      "Responsive Web Design",
-      "Effects & Optimizations"
-    ],
-  },
-  {
-    title: "Omnifood",
-    techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
-    timeSpent: "1 Week",
-    responsive: true,
-    link:"https://lively-kleicha-55527e.netlify.app/",
-    image:Omnifood,
-    projectDescription:
-      "This project was focused on learning best practices with HTML, learning CSS from basics to some advanced technique and after that basics of SASS/SCSS. This project also tought me the techniques to build responsive websites using Grid/Flexbox.",
-    conceptsCovered: [
-      "HTML fundamentals",
-      "CSS fundamentals",
-      "Floats",
-      "Flexbox",
-      "Grids",
-      "Web Design Rules",
-      "Components Pattern",
-      "Layout Patern",
-      "Responsive Web Design",
-      "Effects & Optimizations"
-    ],
-  },
-  {
-    title: "Omnifood",
-    techStack: ["HTML, ", "CSS/SCSS, ", "JavaScript "],
-    timeSpent: "1 Week",
-    responsive: true,
-    link:"https://lively-kleicha-55527e.netlify.app/",
-    image:Omnifood,
+    image:Nexter,
     projectDescription:
       "This project was focused on learning best practices with HTML, learning CSS from basics to some advanced technique and after that basics of SASS/SCSS. This project also tought me the techniques to build responsive websites using Grid/Flexbox.",
     conceptsCovered: [
